5e295c1888a9421363f348789d9ab8de828ebbcc
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-08-05 13:26:05 +0200

    results component added

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,34 @@
   <h1>Vue Reaction Timer</h1>
   <button v-on:click="start" :disabled="isPlaying">PLAY</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
       delay: null,
-      score: null
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.isPlaying = true
+      this.showResults = false
       this.delay = 2000 + Math.random() * 5000
     },
     endGame(reactionTimer) {
       this.score = reactionTimer
       this.isPlaying = false
+      this.showResults = true
     }
   }
 }
---
+++ src/components/Results.vue
@@ -0,0 +1,15 @@
+<template>
+  <p> Your time is: {{ score }} ms.</p>
+</template>
+
+<script>
+export default {
+ props: ['score']
+}
+</script>
+
+<style>
+  p {
+    font-weight: bold;
+  }
+</style>
